6ce2deb0d54705ea503fceaf11a7130240c56e32
ReikoSakamoto <rei.sakamo@gmail.com>
2021-10-16 23:46:57 +0900

    表示崩れなど修正

--- js/script.js
+++ js/script.js
@@ -1,46 +1,70 @@
-$(document).ready(function() {
+$( function($) {
+      //ドロワーメニュー
 
-// スムーススクロール
-    // #から始まるURLがクリックされた時
-    jQuery('a[href^="#"]').click(function() {
-      // .headerクラスがついた要素の高さを取得（ヘッダーに重ならないように）
-      let header = jQuery(".header").innerHeight();
-      // 移動速度を指定（ミリ秒）
-      let speed = 300;
-      // hrefで指定されたidを取得 
-      let id = jQuery(this).attr("href");
-      // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
-      let target = jQuery("#" == id ? "html" : id);
-      // ページのトップを基準にターゲットの位置を取得
-      let position = jQuery(target).offset().top;
-      // ターゲットの位置までspeedの速度で移動
-      jQuery("html, body").animate(
-      {
-          scrollTop: position - $( '#js-header' ).outerHeight()
-      },
-      speed
-      );
+    $(".openbtn1").click(function () {//ボタンがクリックされたら
+      $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+        $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    });
+      
+    $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+        $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+        $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+    });
+
+
+    //wow
+    new WOW().init();
+
+    $('#js-nav a[href*="#"]').click(function() {
+      var elmHash = $(this).attr('href');
+      var pos = $(elmHash).offset().top-50;
+      $('body,html').animate({scrollTop: pos}, 500);
       return false;
-  });
+    });
 
-  jQuery(window).on("scroll", function($) {
-    if (jQuery(this).scrollTop() > 600) {
-      jQuery('.totop').show();
+
+
+    // スムーススクロール
+    // #から始まるURLがクリックされた時
+    // $('a[href^="#"]').click(function() {
+    //   // .headerクラスがついた要素の高さを取得（ヘッダーに重ならないように）
+    //   let header = jQuery(".header").innerHeight();
+    //   // 移動速度を指定（ミリ秒）
+    //   let speed = 300;
+    //   // hrefで指定されたidを取得 
+    //   let id = jQuery(this).attr("href");
+    //   // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
+    //   let target = jQuery("#" == id ? "html" : id);
+    //   // ページのトップを基準にターゲットの位置を取得
+    //   let position = jQuery(target).offset().top;
+    //   // ターゲットの位置までspeedの速度で移動
+    //   $("html, body").animate(
+    //   {
+    //       scrollTop: position - $( '#js-header' ).outerHeight()
+    //   },
+    //   speed
+    //   );
+    //   return false;
+    // });
+
+    $(window).scroll( function() {
+    if ($(this).scrollTop() > 600) {
+      $('.totop').show();
     } else {
-      jQuery('.totop').hide();
+      $('.totop').hide();
     }
-  });
+    });
   
-  jQuery('.totop').click(function () {
-    jQuery('body,html').animate({
+    $('.totop').click(function () {
+      $('body,html').animate({
       scrollTop: 0
-    }, 500);
+      }, 500);
     return false;
-  });
+    });
 
-  // //googleform
-  let $form = $( '#js-form' )
-  $form.submit(function(e) { 
+     //googleform
+    let $form = $( '#js-form' )
+    $form.submit(function(e) { 
     $.ajax({ 
      url: $form.attr('action'), 
      data: $form.serialize(), 
@@ -49,29 +73,17 @@ $(document).ready(function() {
      statusCode: { 
         0: function() { 
           //送信に成功したときの処理 
-          $form.slideUp()
-          $( '#js-success' ).slideDown()
+          $form.slideUp();
+          $( "#js-success" ).slideDown();
         }, 
         200: function() { 
           //送信に失敗したときの処理 
-          $form.slideUp()
-          $( '#js-error' ).slideDown()
+          $form.slideUp();
+          $( "#js-error" ).slideDown();
         }
       } 
     });
     return false; 
   }); 
 
-  //wow
- new WOW().init();
-
- //ハンバーガーメニュー
- $(function() {
-  $('.navbtn').on('click', function () {
-   $(this).toggleClass('close');
-   $('.nav-menu').toggleClass('open');
-  });
- });
-
-})
-
+});
