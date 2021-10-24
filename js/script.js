4fbf6d910e707056c747ba0ba721a6793c765184
ReikoSakamoto <rei.sakamo@gmail.com>
2021-10-24 22:53:41 +0900

    テスト送信

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 $( function($) {
-      //ドロワーメニュー
-
+      //ドロワーメニュー（ボタンを押すと全画面メニュー表示）
     $(".openbtn1").click(function () {//ボタンがクリックされたら
       $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
         $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
     });
-      
     $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
         $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
         $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
     });
 
-
     //wow
     new WOW().init();
 
+    //スムーススクロール
     $('#js-nav a[href*="#"]').click(function() {
       var elmHash = $(this).attr('href');
       var pos = $(elmHash).offset().top-50;
@@ -22,68 +20,19 @@ $( function($) {
       return false;
     });
 
-
-
-    // スムーススクロール
-    // #から始まるURLがクリックされた時
-    // $('a[href^="#"]').click(function() {
-    //   // .headerクラスがついた要素の高さを取得（ヘッダーに重ならないように）
-    //   let header = jQuery(".header").innerHeight();
-    //   // 移動速度を指定（ミリ秒）
-    //   let speed = 300;
-    //   // hrefで指定されたidを取得 
-    //   let id = jQuery(this).attr("href");
-    //   // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
-    //   let target = jQuery("#" == id ? "html" : id);
-    //   // ページのトップを基準にターゲットの位置を取得
-    //   let position = jQuery(target).offset().top;
-    //   // ターゲットの位置までspeedの速度で移動
-    //   $("html, body").animate(
-    //   {
-    //       scrollTop: position - $( '#js-header' ).outerHeight()
-    //   },
-    //   speed
-    //   );
-    //   return false;
-    // });
-
+    //トップへ戻るボタン
     $(window).scroll( function() {
-    if ($(this).scrollTop() > 600) {
-      $('.totop').show();
-    } else {
+      if ($(this).scrollTop() > 600) {
+        $('.totop').show();
+      } else {
       $('.totop').hide();
-    }
+      }
     });
-  
     $('.totop').click(function () {
       $('body,html').animate({
       scrollTop: 0
       }, 500);
     return false;
     });
-
-     //googleform
-    let $form = $( '#js-form' )
-    $form.submit(function(e) { 
-    $.ajax({ 
-     url: $form.attr('action'), 
-     data: $form.serialize(), 
-     type: "POST", 
-     dataType: "xml", 
-     statusCode: { 
-        0: function() { 
-          //送信に成功したときの処理 
-          $form.slideUp();
-          $( "#js-success" ).slideDown();
-        }, 
-        200: function() { 
-          //送信に失敗したときの処理 
-          $form.slideUp();
-          $( "#js-error" ).slideDown();
-        }
-      } 
-    });
-    return false; 
-  }); 
 
 });
